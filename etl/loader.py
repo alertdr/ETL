129968061bd21e25e6999ec76d2075fefc0c4f2e
hinesqui <hinesqui@gmail.com>
2022-04-09 14:22:12 +0300

    add comment about Generator and backoff/add loop

--- etl/loader.py
+++ etl/loader.py
@@ -1,12 +1,13 @@
 import logging
 from collections import deque
 from logging import config
+from time import sleep
 from typing import Generator
 
 from elasticsearch import helpers
 
 from data_workers import PostgresLoader, ElasticsearchLoader, Filmwork
-from etc.config import BATCH_SIZE
+from etc.config import BATCH_SIZE, AWAIT_TIME
 from etl.etc.queries import QUERIES
 from state import State, JsonFileStorage
 from utils import backoff, get_format_time
@@ -14,7 +15,10 @@ from utils import backoff, get_format_time
 
 def extract_data(*, query: str) -> Generator:
     """
-    Функция извлечения данных из postgresql
+    Функция извлечения данных из postgresql, нет возможности применить декортаор backoff() так как
+    генератор инициализируется только в transform_data. В случае возникновения ошибки в extract_data
+    отловить её можно будет только в transform_data. Были предприняты попытки написать еще один декоратор
+    для инициализации генератора, но они не увенчались успехом
 
     :param query: sql запрос
     :yield: item: единичный результат выполнения sql запроса
@@ -73,8 +77,11 @@ def main(*, query_name: str, query: str) -> None:
 if __name__ == '__main__':
     config.fileConfig("etc/logger.conf")
     state = State(storage=JsonFileStorage(file_path='state.json'))
-    for query_name, sql_query in QUERIES.items():
-        logging.info(f'Query {query_name} started')
-        time = get_format_time(time=state.get_state(key=query_name))
-        query = sql_query(time)
-        main(query_name=query_name, query=query)
+    while True:
+        for query_name, sql_query in QUERIES.items():
+            logging.info(f'Query {query_name} started')
+            time = get_format_time(time=state.get_state(key=query_name))
+            query = sql_query(time)
+            main(query_name=query_name, query=query)
+
+        sleep(AWAIT_TIME)

a636cc6a739c35e69cec6b5dc06aabbe86a102fe
hinesqui <hinesqui@gmail.com>
2022-04-09 04:31:59 +0300

    add logging/docstrings

--- etl/data_workers.py
+++ etl/data_workers.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from dataclasses import dataclass
 from typing import Generator, Optional, Union
 
@@ -10,40 +11,79 @@ from etc.config import DSL, ES_CONFIG
 
 
 class PostgresLoader:
+    """
+    Класс для подключения и выполнения запросов к бд postgresql
+    """
+
     def __init__(self, *, fetch_size: int = 300):
         self.fetch_size = fetch_size
 
     def __enter__(self):
+        logging.debug('Connecting to postgres')
         self.connection = psycopg2.connect(**DSL, cursor_factory=DictCursor)
         self.cursor = self.connection.cursor()
+        logging.debug('Postgres connection complete')
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.commit()
         self.connection.close()
+        logging.debug('Postgres connection closed')
 
     def batch_execute(self, *, query: str) -> Generator:
+        """
+        Метод извлечения группы полей
+
+        :param query: sql запрос
+        :yield: rows: группа полей, результат выполнения запроса
+        """
         self.cursor.execute(query)
         while rows := self.cursor.fetchmany(size=self.fetch_size):
+            logging.info(f'Postgres executed: {len(rows)}')
             yield rows
+
+    def get_filmwork_modified(self, *, filmwork_id: str) -> list:
+        """
+        Метод извлечения последнего времени изменения конкретного кинопроизведения по id
+
+        :param filmwork_id: id кинопроизведения
+        :return: результат извлечения времени последнего изменения конкретного кинопроизведения
+        """
+        self.cursor.execute(f"SELECT modified FROM film_work WHERE id = '{filmwork_id}'")
+        return self.cursor.fetchone()
 
 
 class ElasticsearchLoader:
+    """
+    Класс для конфигурирования и подключения к elasticsearch
+    """
+
     def __init__(self):
         self.index_name = ES_CONFIG['index_name']
-        self._mapping = self.load_mapping(ES_CONFIG['movies_settings'])
+        self._mapping = self.load_settings(ES_CONFIG['movies_settings'])
 
     def __enter__(self) -> Elasticsearch:
+        logging.debug('Connecting to elasticsearch')
         self.es = Elasticsearch(ES_CONFIG['hosts'])
         if not self.es.indices.exists(self.index_name):
+            logging.debug(f'Elasticsearch index {self.index_name} does not exists')
             self.es.indices.create(index=self.index_name, body=self._mapping)
+            logging.debug(f'Elasticsearch index {self.index_name} created')
+        logging.debug(f'Elasticsearch connection complete')
         return self.es
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.es.close()
+        logging.debug('Elasticsearch connection closed')
 
     @staticmethod
-    def load_mapping(file_path: str) -> dict:
+    def load_settings(file_path: str) -> dict:
+        """
+        Загрузка настроек для индекса movies
+
+        :param file_path: путь к файлу настроек
+        :return: словарь настроек
+        """
         with open(file_path, 'r') as file:
             return json.load(file)
 
@@ -73,7 +113,12 @@ class Filmwork:
         if not self.director:
             self.director = []
 
-    def get_es_format(self) -> dict:
+    def get_bulk_format(self) -> dict:
+        """
+        Метод возврата словаря для bulk запроса elasticsearch
+
+        :return: словарь для bulk запросов elasticsearch
+        """
         return {
             '_index': ES_CONFIG['index_name'],
             '_id': self.id,
--- etl/state.py
+++ etl/state.py
@@ -1,5 +1,6 @@
 import abc
 import json
+import logging
 import os
 from typing import Any, Optional
 
@@ -15,7 +16,11 @@ class BaseStorage:
 
 
 class JsonFileStorage(BaseStorage):
-    def __init__(self, file_path: Optional[str] = None):
+    """
+    Класс для хранения состояния в файле.
+    """
+
+    def __init__(self, *, file_path: Optional[str] = None):
         self.file_path = file_path
 
     def retrieve_state(self) -> dict:
@@ -23,13 +28,15 @@ class JsonFileStorage(BaseStorage):
         if self.file_path is not None and os.path.exists(self.file_path):
             with open(self.file_path, 'r') as file:
                 state = file.readline()
+            logging.debug(f'State: {state} retrieved')
             return json.loads(state)
 
         return state
 
-    def save_state(self, state: dict) -> None:
+    def save_state(self, *, state: dict) -> None:
         with open(self.file_path, 'w') as file:
             file.write(json.dumps(state))
+        logging.debug(f'State: {state} saved')
 
 
 class State:
@@ -39,13 +46,14 @@ class State:
     В целом ничего не мешает поменять это поведение на работу с БД или распределённым хранилищем.
     """
 
-    def __init__(self, storage: BaseStorage):
+    def __init__(self, *, storage: BaseStorage):
         self.storage = storage
 
-    def set_state(self, key: str, value: Any) -> None:
+    def set_state(self, *, key: str, value: Any) -> None:
         state = self.storage.retrieve_state()
         state[key] = value
-        self.storage.save_state(state)
+        logging.debug(f'State query: {key}: {value} set')
+        self.storage.save_state(state=state)
 
-    def get_state(self, key: str) -> Any:
+    def get_state(self, *, key: str) -> Any:
         return self.storage.retrieve_state().get(key)
